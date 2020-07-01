b3adadcc97c87c5a7127f4a023801e7ae51656ee
Michal Knizek <michal.knizek@tado.com>
2020-07-01 15:22:12 +0200

    update grecent and jdk functions

--- functions/grecent.fish
+++ functions/grecent.fish
@@ -1,3 +1,3 @@
 function grecent
-  git reflog | grep -E "to (feat|fix|chore)" | cut -d' ' -f8 | head $argv | awk '!x[$0]++'
+  git reflog | cut -d' ' -f8 | grep - | head $argv | awk '!x[$0]++'
 end
--- functions/jdk.fish
+++ functions/jdk.fish
@@ -1,4 +1,5 @@
 function jdk --description 'Switch jdk' --no-scope-shadowing
   set -x JAVA_HOME (/usr/libexec/java_home -v$argv)
+  java -version
 end
 
